76736090411e0a901ce3423632be9d9a4150e870
TiyoSA <tiyosapriansya88@gmail.com>
2025-05-27 23:31:34 +0700

    Perbaikan1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-// Layouts
+// Layout
 import LayoutPenyewa from '../layouts/LayuotPenyewa.vue'
 import LayoutPemilik from '../layouts/LayoutPemilik.vue'
 
